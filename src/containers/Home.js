491a9862be37b92b58eec6fce10849dfa2aeb333
Lechchi14 <Andira1402Aulia@Gmail.com>
2020-12-30 15:32:59 +0700

    Changing the layout of the web

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,11 +4,18 @@ import React, { useState, useEffect, useMemo } from "react";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
+import City from "../components/City";
 
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
-  // TODO
+  return (
+  <div className="flex flex-col h-screen bg-green-200"> 
+      <City cityName="Yokohama" temp="20°C" color="bg-pink-500"/>
+      <City cityName="Shibuya" temp="22°C" color="bg-yellow-500"/>
+      <City cityName="Tokyo" temp="18°C" color="bg-blue-500"/>
+    </div>
+  );
 }
 
 export default Home;
