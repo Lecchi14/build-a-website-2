cb14789f410712e21038148fd30c35b08721190f
Lechchi14 <Andira1402Aulia@Gmail.com>
2020-12-30 13:31:43 +0700

    message here

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
         <h1> TODO </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Tokyo"> Tokyo </a>
+        <a href="/?city=New%20York%20City"> New York City </a>
+        <a href="/?city=Texas"> Texas </a>
+        <a href="/?city=Shibuya"> Shibuya </a>
       </nav>
     </header>
   );
